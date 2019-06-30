1be8556b4eaa951b75eed19b8e9708eec694684d
GrimLink <sean.grimlink@gmail.com>
2019-06-30 19:14:02 +0200

    ADD: installer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 30-06-19
+* ADD: installer
+
 ## 05-05-19
 * FIX: empty custom URL in create magento
 * IMP: formating
--- install.sh
+++ install.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-# TODO: create more options for install
+RESET='\033[0m'
+BOLD='\033[1m'
+ITALIC='\033[3m'
+GREEN='\033[1;32m'
 
-if [ ! -d ~/bin ]; then
-  mkdir ~/bin
-fi
+# Make sure our ouput folder exists
+if [ ! -d ~/bin ]; then mkdir ~/bin; fi
+if [ ! -d ~/bin/create-bin ]; then mkdir ~/bin/create-bin; fi
 
-rsync \
-  --exclude ".git" \
-  --exclude ".DS_Store" \
-  --exclude "*.md" \
-  --exclude "*.sh" \
-  -avh --no-perms \
-  "$(dirname "${BASH_SOURCE}")" ~/bin
+# Create temp
+mkdir ~/bin/create-project-temp && cd ~/bin/create-project-temp
+
+# Get task runner
+echo -e "${GREEN}Installing create-project${RESET}"
+curl -sS -O "https://raw.githubusercontent.com/GrimLink/create-project/master/create-project" &&
+cp create-project ~/bin/create-project &&
+chmod +x ~/bin/create-project
+
+# Get each task
+BINTASKS=(
+  'magento'
+  'wordpress'
+  'react'
+  'vue'
+  'october'
+  'jigsaw'
+  'html'
+);
+
+echo -e "Getting the tasks.."
+for i in "${BINTASKS[@]}"; do
+  curl -sS -O "https://raw.githubusercontent.com/GrimLink/create-project/master/create-bin/${i}" &&
+  cp $i ~/bin/create-bin/$i
+  echo -e " [${GREEN}✓${RESET}] ${i}"
+done
+
+# Remove temp
+rm -r ~/bin/create-project-temp
+
+# Done message
+echo ""
+echo -e "${GREEN}Done${RESET} 🎉"
+echo "For info on how to use create-project"
+echo "Read it here → https://github.com/GrimLink/create-project/blob/master/README.md"
